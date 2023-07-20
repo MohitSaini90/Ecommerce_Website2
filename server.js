5de23385a3ea8faedf271b807eedd04ca1d4c8bc
MohitSaini90 <97613863+MohitSaini90@users.noreply.github.com>
2023-07-20 23:09:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ import cors from "cors";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js";
 import path from "path";
-
+import { fileURLToPath} from "url";
 const app = express();
 
 //configure
 dotenv.config();
 connectDB();
+
+//ESModule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //For forgot password UI
 app.set("view engine", "ejs");
