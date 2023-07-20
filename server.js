7fb0c68bd431063e9b1307f2e45a604c4d3b3ea4
MohitSaini90 <97613863+MohitSaini90@users.noreply.github.com>
2023-07-20 23:04:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-//app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 //Routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", categoryRoute);
